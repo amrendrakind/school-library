d9ad7f0794d567a300524434a8bbc46a78b58707
amrendrakind <vision2010@gmail.com>
2022-06-24 03:38:21 +0530

    Add class book and rental and Update class person

---
+++ book.rb
@@ -0,0 +1,13 @@
+require './rental'
+class Book
+    attr_accessor :title, :author
+    attr_reader :rentals
+
+    def initialize(title, author)
+        @title = title
+        @author = author
+    end
+    def add_rental(date, person)
+        Rental.new(date, self, person)    
+    end
+end
