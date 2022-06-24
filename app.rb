3a90c05e5ad646f9b84b5ebfc5c1533ed7ff7c04
amrendrakind <vision2010@gmail.com>
2022-06-25 02:06:18 +0530

    Update: calling App Methods in main

--- app.rb
+++ app.rb
@@ -12,18 +12,6 @@ class App
     @rentals = []
   end
 
-  def select(input)
-    case input
-    when 1 then list_books
-    when 2 then list_people
-    when 3 then create_person
-    when 4 then create_book
-    when 5 then create_rental
-    when 6 then list_rentals
-    when 7 then exit
-    end
-  end
-
   def list_books
     puts "list_books"
   end
